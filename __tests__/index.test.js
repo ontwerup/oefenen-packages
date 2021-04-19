a2a37a9776316e9c5904f938c351a262f2cff30a
ontwerup <info@ontwerup.nl>
2021-04-19 16:59:10 +0200

    index.js toegevoegd

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,27 @@
-test("Controleer of de test wel werkt", ()=>  {
-    console.log("Ik word uitgevoerd")
+
+function add (num1, num2) {
+    return num1 + num2;
+    
+}
+
+
+test("The add function add 2 numbers and returns the sum", ()=>  {
+
+    // Arrange (=klaarzetten)
+    const number1 = 5;
+    const number2 = 10;
+
+
+
+    // Act (=hoe moet het gaan handelen?)
+    const sum = add (number1, number2);
+
+
+
+    // Assert (=beweren)
+    expect(sum).toBe(15);
+
+
+
 })
 
